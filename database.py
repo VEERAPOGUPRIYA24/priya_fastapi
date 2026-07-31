a31437711b986063be462e604de53355ba85e4b8
VEERAPOGUPRIYA24 <veerapogupriya@gmail.com>
2026-07-31 21:23:35 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/mobiles_db"
+#DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/mobiles_db"
+DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_-VkdD3d926_ZR6fzHhF@priya24-veerapogupriya-51fe.h.aivencloud.com:12834/defaultdb"
 
 engine = create_engine(DATABASE_URL)
 
